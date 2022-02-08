71a497c30759376e72a2d8a8dd56e23dce95df00
Kendrick Smith <kmsmith@perimeterinstitute.ca>
2022-02-08 12:30:43 -0500

    constexpr_min(), constexpr_max()

--- include/gputils/constexpr_functions.hpp
+++ include/gputils/constexpr_functions.hpp
@@ -2,6 +2,7 @@
 #define _GPUTILS_CONSTEXPR_FUNCTIONS_HPP
 
 
+// Frequently used in static_assert().
 constexpr __host__ __device__ bool constexpr_is_divisible(int m, int n)
 {
     return (m >= 0) && (n > 0) && ((m % n) == 0);
@@ -21,5 +22,19 @@ constexpr __host__ __device__ int constexpr_ilog2(int n)
     
     return (n > 1) ? (constexpr_ilog2(n/2)+1) : 0;
 }
+
+
+template<typename T>
+constexpr __host__ __device__ T constexpr_min(T m, T n)
+{
+    return (m < n) ? m : n;
+}
+
+template<typename T>
+constexpr __host__ __device__ T constexpr_max(T m, T n)
+{
+    return (m > n) ? m : n;
+}
+
 
 #endif // _GPUTILS_CONSTEXPR_FUNCTIONS_HPP
