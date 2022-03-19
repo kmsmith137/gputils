30b172f89c676f1696060d904ce678b491cdb978
Kendrick Smith <kmsmith@perimeterinstitute.ca>
2022-03-19 10:24:04 -0400

    a little more work on benchmarks/tensor-cores.cu

--- benchmarks/tensor-cores.cu
+++ benchmarks/tensor-cores.cu
@@ -1,11 +1,16 @@
 #include <sstream>
 #include <iostream>
+
 #include "../include/gputils/Array.hpp"
 #include "../include/gputils/CudaStreamPool.hpp"
 #include "../include/gputils/cuda_utils.hpp"
 #include "../include/gputils/device_mma.hpp"
 
+// C++ WMMAs
+#include <mma.h>
+
 using namespace std;
+using namespace nvcuda;
 using namespace gputils;
 
 
@@ -214,6 +219,83 @@ static void time_int_mma(int niter, int num_active_warps=32)
 
 
 // -------------------------------------------------------------------------------------------------
+//
+// C++ WMMA kernel
+
+
+// The 'asrc' array length is (nwarps*32).
+// The 'bsrc' array length is (nwarps*32).
+// The 'csrc' array length is (nwarps*64).
+//
+// Here, nwarps = (nblocks * nwarps_per_block) is the total number of warps in the kernel.
+
+__global__ void cpp_int4_kernel(int *cdst, const int *asrc, const int *bsrc, int niter, int num_active_warps)
+{
+    int warpId = threadIdx.x >> 5;
+    int iwarp = (blockIdx.x * blockDim.x + threadIdx.x) >> 5;
+
+    if (warpId >= num_active_warps)
+	return;
+
+    wmma::fragment<wmma::matrix_a, 8, 8, 32, wmma::experimental::precision::s4, wmma::row_major> a_frag;
+    wmma::fragment<wmma::matrix_b, 8, 8, 32, wmma::experimental::precision::s4, wmma::col_major> b_frag;
+    wmma::fragment<wmma::accumulator, 8, 8, 32, int> c_frag;
+
+    wmma::load_matrix_sync(a_frag, asrc + 32*iwarp, 32);
+    wmma::load_matrix_sync(b_frag, bsrc + 32*iwarp, 32);
+    wmma::fill_fragment(c_frag, 0);
+
+    for (int i = 0; i < niter; i++)
+	wmma::mma_sync(c_frag, a_frag, b_frag, c_frag);
+
+   wmma::store_matrix_sync(cdst + 64*iwarp, c_frag, 8, wmma::mem_row_major);
+}
+
+
+static void time_cpp_int4_mma(int niter, int num_active_warps=32)
+{
+    const int nblocks = 82 * 84;
+    const int nthreads_per_block = 1024;
+    const int ncallbacks = 15;
+    const int nstreams = 2;
+
+    int nwarps = nblocks * (nthreads_per_block / 32);
+    Array<int> asrc({nstreams,32*nwarps}, af_zero | af_gpu);
+    Array<int> bsrc({nstreams,32*nwarps}, af_zero | af_gpu);
+    Array<int> cdst({nstreams,64*nwarps}, af_zero | af_gpu);
+
+    const int M = 8;
+    const int N = 8;
+    const int K = 32;
+    double flops_per_mma = 2*M*N*K;
+    double tflops_per_kernel = double(niter) * nblocks * num_active_warps * flops_per_mma / pow(2,40.);
+
+    auto callback = [&](const CudaStreamPool &pool, cudaStream_t stream, int istream)
+	{
+	    int *a = asrc.data + istream*32*nwarps;
+	    int *b = bsrc.data + istream*32*nwarps;
+	    int *c = cdst.data + istream*64*nwarps;
+
+	    cpp_int4_kernel
+		<<< nblocks, nthreads_per_block, 0, stream >>>
+		(c, a, b, niter, num_active_warps);
+
+	    CUDA_PEEK("mma_cpp_int4_kernel");
+	};
+
+    stringstream ss;
+    ss << "C++ int4 (m=8, n=8, k=32)";
+
+    if (num_active_warps < 32)
+	ss << " **ACTIVE_WARPS=" << num_active_warps << "**";
+    
+    CudaStreamPool pool(callback, ncallbacks, nstreams, ss.str());
+    pool.monitor_throughput("Tflops", tflops_per_kernel);
+    pool.run();
+}
+
+
+// -------------------------------------------------------------------------------------------------
 
 
 static void time_mmas(int num_active_warps=32)
@@ -231,6 +313,9 @@ static void time_mmas(int num_active_warps=32)
     time_int_mma <mma_s4_m8_n8_k32, 4, 8, 8, 32> (2048*1024/num_active_warps, num_active_warps);
     time_int_mma <mma_s4_m16_n8_k32, 4, 16, 8, 32> (1024*1024/num_active_warps, num_active_warps);
     time_int_mma <mma_s4_m16_n8_k64, 4, 16, 8, 64> (512*1024/num_active_warps, num_active_warps);
+
+    // C++ int4
+    time_cpp_int4_mma(2048*1024/num_active_warps, num_active_warps);
 }
 
 
@@ -238,6 +323,7 @@ int main(int argc, char **argv)
 {
     time_mmas(32);  // full occupancy (32 warps/SM)
     time_mmas(8);   // low occupancy (8 warps/SM)
+    time_mmas(4);   // even lower occupancy (4 warps/SM)
     
     return 0;
 }
